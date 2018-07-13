2b448e81bd2a2c4321a4d22d10a8f559406316ac
ERIC KELLY <eakelly220@gmail.com>
2018-07-13 00:14:16 +0000

    automatically backed up by learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -2,8 +2,8 @@ class Puppies
   attr_accessor :name, :breed, :age
 
   def initialize(name,breed,age)
-    @name = name 
-    @breed = breed 
+    @name = name
+    @breed = breed
     @age = age
-  end 
+  end
 end
